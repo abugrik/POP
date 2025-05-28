9937f4237e41c39c21482e0363b96e683941cd44
abugrik <israilov1608@gmail.com>
2025-05-28 23:39:28 +0600

    Add files via upload

---
+++ abu.py
@@ -0,0 +1,9 @@
+number = 3
+
+abu = 12345
+word ="Результат:"
+print("word", abu)
+
+number = 7
+
+print("Результат:",number)
---
+++ abuu.py
@@ -0,0 +1,8 @@
+num1 =input("Введите первое число: ")
+
+num2 =input("Введите второе число: ")
+
+print ("Result:", num1 + num2 )
+print ("Result:", num1 - num2 )
+print ("Result:" ,num1 / num2 )
+print ("Result:", num1 * num2 )
